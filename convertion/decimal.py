43c2b1cdcbd671f427b53ed391129b9643af80a1
fcarrascop <carrascopatelli.fa@gmail.com>
2026-03-17 09:37:29 -0300

    Funcion decimal lista + arreglo errores strings.py

--- convertion/decimal.py
+++ convertion/decimal.py
@@ -1,11 +1,44 @@
-# from typing import
+from utils.strings import stringANumero
 
 def decimal(tipo: str, texto: str) -> str | None:
 	if tipo == "*":
-		return "Binario"
+		# Binario
+		# División repetida
+		numero = stringANumero(texto)
+		final = ""
+
+		while numero > 0:
+			resto = numero % 2
+			final += "01"[resto]
+			numero //= 2
+
+		return final[::-1]
 	elif tipo == "&":
-		return "Octal"
+		# Octal
+		# División repetida
+		numero = stringANumero(texto)
+		final = ""
+
+		while numero > 0:
+			resto = numero % 8
+			final += "01234567"[resto]
+			numero //= 8
+
+		return final[::-1]
 	elif tipo == "!":
-		return "Hexa"
+		# Hexadecimal
+		# División repetida
+
+		numero = stringANumero(texto)
+		final = ""
+
+		while numero > 0:
+			resto = numero % 16
+			final += "0123456789ABCDEF"[resto]
+			numero //= 16
+
+		return final[::-1]
 	else:
 		return None
+
+print(decimal("!", "255"))
